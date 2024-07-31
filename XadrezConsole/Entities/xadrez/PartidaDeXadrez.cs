6eb09bd5fdb5b26e70c2218bfafbed6ca6f498f7
João Pedro <jaopedro_santos@hotmail.com>
2024-07-30 21:29:41 -0300

    Jogada especial promocao

--- XadrezConsole/Entities/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/Entities/xadrez/PartidaDeXadrez.cs
@@ -90,6 +90,22 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = Tabuleiro.Peca(destino);
+
+            // #jogadaEspecial Promoção
+            if (p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tabuleiro.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca Dama = new Dama(Tabuleiro, p.Cor);
+                    Tabuleiro.ColocarPeca(Dama, destino);
+                    Pecas.Add(Dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -108,8 +124,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tabuleiro.Peca(destino);
 
             // #jogadaEspecial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
